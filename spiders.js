8fb8fa8889355a9d212c012399e7c4e4b660e9ed
Sklipkos <sklipkos@gmail.com>
2026-03-13 20:47:58 +0100

    website uptade

--- spiders.js
+++ spiders.js
@@ -1,51 +1,33 @@
 const spiders = [
-
-{
-name:"Heteroscodra maculata",
-size:"samice • 3 cm",
-price:"700 Kč",
-image:"img/spiders/heteroscodra-maculata.jpg",
-link:"species/heteroscodra-maculata.html"
-},
-
-{
-name:"Pterinochilus murinus RCF",
-size:"samice • adult",
-price:"1000 Kč",
-link:"species/pterinochilus-murinus.html"
-},
-
-{
-name:"Lasiodora parahybana",
-size:"samec • 4 cm",
-price:"300 Kč",
-link:"species/lasiodora-parahybana.html"
-}
-
+  {
+    name:"Heteroscodra maculata",
+    size:"samice • 3 cm",
+    price:"700 Kč",
+    image:"img/spiders/heteroscodra-maculata.jpg",
+    link:"species/heteroscodra-maculata.html"
+  },
+  {
+    name:"Pterinochilus murinus RCF",
+    size:"samice • adult",
+    price:"1000 Kč",
+    link:"species/pterinochilus-murinus.html"
+  },
+  {
+    name:"Lasiodora parahybana",
+    size:"samec • 4 cm",
+    price:"300 Kč",
+    link:"species/lasiodora-parahybana.html"
+  }
 ]
 
-const container = document.getElementById("spiderList")
+const container = document.getElementById("spiderList");
 
-spiders.forEach(spider=>{
-
-container.innerHTML += `
-
-<div class="spider">
-
-<a href="${spider.link}">
-${spider.name}
-</a>
-
-<div class="info">
-${spider.size}
-</div>
-
-<div class="price">
-${spider.price}
-</div>
-
-</div>
-
-`
-
-})
+spiders.forEach(spider => {
+  container.innerHTML += `
+  <a href="${spider.link}" class="spider">
+    <div class="name">${spider.name}</div>
+    <div class="info">${spider.size}</div>
+    <div class="price">${spider.price}</div>
+  </a>
+  `;
+});
